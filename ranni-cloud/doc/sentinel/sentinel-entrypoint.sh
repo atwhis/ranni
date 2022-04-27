d5c7bfb6574b121157e44f62aa2086c8238e980d
yuanming.chen <yuanming.chen@ucarinn.com>
2022-04-27 11:29:16 +0800

    modify sentinel dockerfile

--- ranni-cloud/doc/sentinel/sentinel-entrypoint.sh
+++ ranni-cloud/doc/sentinel/sentinel-entrypoint.sh
@@ -10,11 +10,8 @@ JAVA_OPT="${JAVA_OPT} -Dsentinel.dashboard.auth.password=${PASSWORD} "
 JAVA_OPT="${JAVA_OPT} -Dcsp.sentinel.dashboard.server=${SERVER_HOST}:${SERVER_PORT} "
 
 echo "JAVA_OPT============"
-echo "JAVA_OPT============"
-
 echo $JAVA_OPT
 
 java ${JAVA_OPT} -jar /opt/sentinel-dashboard-1.8.1.jar
 
-echo "JAVA_OPT============"
 
