300a8879054274019f5eb5c0dd1d7ccaa12b5344
ymchen <ymchen@demo.com>
2022-08-16 17:34:35 +0800

    oss feature modify

---
+++ ranni-component/ranni-component-oss-starter/src/main/java/com/ymchen/ranni/component/ossstarter/util/OssFileUtil.java
@@ -0,0 +1,24 @@
+package com.ymchen.ranni.component.ossstarter.util;
+
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.UUID;
+
+public class OssFileUtil {
+
+    private OssFileUtil() {
+
+    }
+
+    public static String getFilenameByFileUrl(String fileUrl, String separateStr) {
+        if (StringUtils.isNotBlank(separateStr)) {
+            return fileUrl.substring(fileUrl.lastIndexOf(separateStr) + 1);
+        }
+        return fileUrl;
+    }
+
+    public static String generateNewFilename(String filename) {
+        return UUID.randomUUID().toString().replaceAll("-", "")
+                + filename.substring(filename.lastIndexOf("."));
+    }
+}
