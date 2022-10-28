0bda95ffa5307d779029742925f5cde437b3ec11
ymchen <ymchen@demo.com>
2022-10-28 11:04:50 +0800

    xxl-job starter

---
+++ ranni-component/ranni-component-xxljob-starter/src/main/java/com/ymchen/ranni/component/xxljob/config/XXLJobAutoConfig.java
@@ -0,0 +1,26 @@
+package com.ymchen.ranni.component.xxljob.config;
+
+import com.xxl.job.core.executor.impl.XxlJobSpringExecutor;
+import com.ymchen.ranni.component.xxljob.properties.XXLJobProperties;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+@EnableConfigurationProperties(XXLJobProperties.class)
+public class XXLJobAutoConfig {
+
+    @Bean
+    @ConditionalOnProperty(name = "xxl.job.enable",havingValue = "true")
+    public XxlJobSpringExecutor xxlJobExecutor(XXLJobProperties xxlJobProperties) {
+        XxlJobSpringExecutor xxlJobSpringExecutor = new XxlJobSpringExecutor();
+        xxlJobSpringExecutor.setAdminAddresses(xxlJobProperties.getAdmin().getAddress());
+        xxlJobSpringExecutor.setAppname(xxlJobProperties.getExecutor().getAppname());
+        xxlJobSpringExecutor.setAddress(xxlJobProperties.getExecutor().getAddress());
+        xxlJobSpringExecutor.setIp(xxlJobProperties.getExecutor().getIp());
+        xxlJobSpringExecutor.setPort(xxlJobProperties.getExecutor().getPort());
+        xxlJobSpringExecutor.setAccessToken(xxlJobProperties.getAccessToken());
+        xxlJobSpringExecutor.setLogPath(xxlJobProperties.getExecutor().getLogPath());
+        xxlJobSpringExecutor.setLogRetentionDays(xxlJobProperties.getExecutor().getLogRetentionDays());
+        return xxlJobSpringExecutor;
+    }
+}
